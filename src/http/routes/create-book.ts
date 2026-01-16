79934f4d822c512eec3d256e6a740cdba856c975
Gustavo Silva <Gustavo.katia.rodrigues@gmail.com>
2026-01-15 20:10:12 -0400

    feat: criando a base de um get com o node

--- src/http/routes/create-book.ts
+++ src/http/routes/create-book.ts
@@ -2,6 +2,27 @@ import { FastifyInstance } from "fastify";
 import { z } from "zod";
 import { prisma } from "../lib/prisma.js";
 
-export function createBookroute(app: FastifyInstance) {
-  app.post("/books", async (request, reply) => {});
+export async function createBookroute(app: FastifyInstance) {
+  app.post("/books", async (request, reply) => {
+    const createBoockBody = z.object({
+      title: z.string(),
+      author: z.string(),
+      description: z.string(),
+    });
+
+    const { title, author, description } = createBoockBody.parse(request.body);
+
+    const book = await prisma.book.create({
+      data: {
+        title,
+        author,
+        description,
+      },
+    });
+    if (book) {
+      return reply.status(201).send({ bookId: book.id });
+    } else {
+      return reply.status(500).send({ error: "Failed to create book" });
+    }
+  });
 }
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,9 @@
 import fastify from "fastify";
+import { createBookroute } from "./routes/create-book";
 
 const app = fastify();
 
-app.get("/", () => {
-  return { message: "Hello, World!" };
-});
+app.register(createBookroute);
 
 app.listen({ port: 3000 }).then(() => {
   console.log("Server is running on ");
